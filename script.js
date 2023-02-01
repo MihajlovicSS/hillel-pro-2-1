4f7658c92482b2dfa6cd12e681cd41e47198ce99
MihajlovicSS <mihajlovicsinisa1456780@gmail.com>
2023-02-01 15:52:55 +0200

    second version

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 function printHello() {
-    let userName = prompt('What is your name?');
-    alert('Hello ' + userName + '! How are you?');
+    let userName;
+    for(let i=0; i<=5; i++){
+        if (i===0){
+            userName = prompt('Please enter your name!');
+            if(checkLetters(userName)){
+                alert('Hello, ' + userName + '! How are you?'); 
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+        else if (i===5){
+            userName = prompt('Please enter correct name! Only letters! You have ' + (6 - i) + ' attempts remained!');
+            if(checkLetters(userName)){
+                alert('Hello ' + userName + '! How are you?'); 
+                break;
+            }
+            else{
+                alert('You have could not enter the correct name! Sorry!');
+            }
+        }
+        else{
+            userName = prompt('Please enter correct name! Only letters! You have ' + (6 - i) + ' attempts remained!');
+            if(checkLetters(userName)){
+                alert('Hello, ' + userName + '! How are you?'); 
+                break;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+}
+function checkLetters(name){
+    let flag = true; // word
+    if (name.length){
+        for(let i=0; i<name.length-1; i++){
+            if (isNaN(name[i]))
+                continue;
+            else
+                flag = false; // number
+                break;
+        }
+    }
+    else flag = false;
+    return flag;
 }
 printHello();
